6ae1a4dbbd29b6f4415fe48bb1a31e1112aaa6f0
DLT-Solutions-JBoss <rick.stewart@dlt.com>
2014-07-30 10:54:39 -0400

    Update ObjectFactory.java

--- src/main/java/com/dlt/weather/model/ObjectFactory.java
+++ src/main/java/com/dlt/weather/model/ObjectFactory.java
@@ -17,7 +17,7 @@ import javax.xml.namespace.QName;
 /**
  * This object contains factory methods for each 
  * Java content interface and Java element interface 
- * generated in the org.jboss.as.quickstarts.hellosport.model package. 
+ * generated in the com.dlt.weather.model package. 
  * <p>An ObjectFactory allows you to programatically 
  * construct new instances of the Java representation 
  * for XML content. The Java representation of XML 
