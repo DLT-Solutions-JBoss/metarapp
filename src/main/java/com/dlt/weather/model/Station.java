036d59f4295ee4bf598502594b4669dc27a14bf3
DLT-Solutions-JBoss <rick.stewart@dlt.com>
2014-07-30 10:57:00 -0400

    Create Station.java

---
+++ src/main/java/com/dlt/weather/model/Station.java
@@ -0,0 +1,71 @@
+package com.dlt.weather.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
+@Entity
+@Table(name="station")
+public class Station {
+
+    @Id
+    @Column(name = "id")
+    String id;
+    
+    @Column(name = "state")
+    String state;
+    
+    @Column(name = "name")
+    String name;
+    
+    @Column(name = "latitude")
+    double  latitude;
+    
+    @Column(name = "longitude")
+    double  longitude;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public double getLatitude() {
+		return latitude;
+	}
+
+	public void setLatitude(double latitude) {
+		this.latitude = latitude;
+	}
+
+	public double getLongitude() {
+		return longitude;
+	}
+
+	public void setLongitude(double longitude) {
+		this.longitude = longitude;
+	}
+    
+    
+}
