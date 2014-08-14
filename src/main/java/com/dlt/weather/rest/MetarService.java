3fa0b935f3b559a4835210bc3a6569ee2be9638b
DLT-Solutions-JBoss <rick.stewart@dlt.com>
2014-08-14 10:11:49 -0400

    Update MetarService.java

--- src/main/java/com/dlt/weather/rest/MetarService.java
+++ src/main/java/com/dlt/weather/rest/MetarService.java
@@ -31,7 +31,7 @@ public class MetarService implements WeatherService{
 
   //      @WebServiceRef(wsdlLocation="http://aviationweather.gov/adds/schema/metar1_2.xsd")
   //      static AviationService service;
-    
+
         @WeatherService(ServiceType.METAR)
         @PersistenceContext(unitName="metarapp", type=PersistenceContextType.EXTENDED)
         private EntityManager emStation;
@@ -44,52 +44,52 @@ public class MetarService implements WeatherService{
         {
                 List <METAR> metar = null;
 
-           	    try 
+           	    try
            	    {
-       	                	       
+
            	       URL myURL = new URL("http://www.aviationweather.gov/adds/dataserver_current/httpparam?dataSource=metars&requestType=retrieve&format=xml&hoursBeforeNow=4&stationString="+sStationId);
            	       HttpURLConnection myURLConnection = (HttpURLConnection) myURL.openConnection();
- 
+
            	       // just want to do an HTTP GET here
            	       myURLConnection.setRequestMethod("GET");
-           	       
+
            	       // uncomment this if you want to write output to this url
            	       myURLConnection.setDoOutput(true);
-           	       
+
            	       // give it 15 seconds to respond
            	       myURLConnection.setReadTimeout(15*1000);
            	       myURLConnection.connect();
-           	      
+
            	       // wrap the urlconnection in a bufferedReader
            	       BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(myURLConnection.getInputStream()));
                    StringBuffer responseXmlString = new StringBuffer();
                    String xmlString;
-                   
-                   while ((xmlString = bufferedReader.readLine()) != null) 
+
+                   while ((xmlString = bufferedReader.readLine()) != null)
                    {
                 	   responseXmlString.append(xmlString);
                    }
                    bufferedReader.close();
-                   
+
                    System.out.println("Returned XML String = "+responseXmlString.toString());
-                   
+
          		   JAXBContext jaxbContext = JAXBContext.newInstance(Response.class);
-          
+
          		   Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
          		   StringReader reader = new StringReader(responseXmlString.toString());
          		   Response metarResponse = (Response) jaxbUnmarshaller.unmarshal(reader);
          		   metar = metarResponse.getData().getMETAR();
-          
-         	    } 
-           	    catch (JAXBException e) 
+
+         	    }
+           	    catch (JAXBException e)
          	    {
          		    e.printStackTrace();
          	    }
-           	    catch (Exception e) 
+           	    catch (Exception e)
          	    {
          		    e.printStackTrace();
-         	    }                
-                
+         	    }
+
                 return metar;
         }
 
@@ -99,30 +99,44 @@ public class MetarService implements WeatherService{
         @WeatherService(ServiceType.METAR)
         public List<Station> getStation(@PathParam("stationId") String sStationId)
         {
-     
+
                 Query query = emStation.createQuery("FROM com.dlt.weather.model.Station where id = ?1 order by name");
                 query.setParameter(1,sStationId);
                 @SuppressWarnings("unchecked")
                 List <Station> station = query.getResultList();
-                
+
                 return station;
         }
 
-        
+        @GET()
+        @Path("station/state/{stateCode}")
+        @Produces("application/json")
+        @WeatherService(ServiceType.METAR)
+        public List<Station> getStationsByState(@PathParam("stateCode") String sStateCode)
+        {
+
+                Query query = emStation.createQuery("FROM com.dlt.weather.model.Station where state = ?1 order by name");
+                query.setParameter(1,sStateCode);
+                @SuppressWarnings("unchecked")
+                List <Station> station = query.getResultList();
+
+                return station;
+        }
+
         @GET()
         @Path("stations")
         @Produces("application/json")
         @WeatherService(ServiceType.METAR)
         public List<Station> getStations()
         {
-     
+
                 Query query = emStation.createQuery("FROM com.dlt.weather.model.Station order by name");
                 @SuppressWarnings("unchecked")
                 List <Station> station = query.getResultList();
-                
+
                 return station;
         }
-        
+
         @Override
         public Class<? extends Annotation> annotationType() {
                 return null;
@@ -132,3 +146,4 @@ public class MetarService implements WeatherService{
                 return null;
         }
 }
+
